4d6ed8294381c44565bbad8a9a1efdcbe1ad35a5
Nebelschwimmer <nebelschwimmer@gmail.com>
2022-12-15 15:53:24 +0300

    Предпоследняя версия

--- js/animations.js
+++ js/animations.js
@@ -22,5 +22,63 @@ function initIncreaseNumberAnimation() {
   increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
+
+
+
+
+
+
+
+//Выбор цены на другое
+
+
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+  if (event.target.value === 'other') {
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input'); // Задание 1
+ 
+    const input = document.createElement('input');
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text"; // Задание 2
+      
+    formContainer.appendChild(input);
+    document.querySelector(".form .form__submit").before(formContainer);; // Задание 3
+  }
+ 
+  const otherInput = document.querySelector('.form__other-input');
+  if (event.target.value !== 'other' && otherInput) { // Задание 5
+    otherInput.remove() // Задание 4
+  }
+});
+
+// Функция скролла
+
+let animationInited = false;
+function updateScroll() {
+  window.addEventListener('scroll', updateScroll)
+  if (window.scrollY > 0) {
   
+  document.querySelector('header').classList.add('header__scrolled')}
+  else {
+    document.querySelector('header').classList.remove('header__scrolled')
+
+  }
+  // Запуск анимации увеличения числа 
+  
+  let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+  let windowBottomPosition = window.scrollY + window.innerHeight;
+ 
+  if (windowBottomPosition >= countElementPosition && !animationInited) {
+    animationInited = true;
+    initIncreaseNumberAnimation()}
+
+
+
+
+}
+
+
+
+
+updateScroll()
